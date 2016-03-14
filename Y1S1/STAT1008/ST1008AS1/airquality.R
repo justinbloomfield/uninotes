8a3df8c76f8746eb504307fc38bb2580a706c368
Justin Bloomfield <jbloomfield@live.com>
2016-03-14 17:16:38 +1100

    Part 1c almost complete, added pdfs of two plots

--- Y1S1/STAT1008/ST1008AS1/airquality.R
+++ Y1S1/STAT1008/ST1008AS1/airquality.R
@@ -1,5 +1,8 @@
 setwd("~/usr/doc/ST1008AS1")
+setwd("/Users/j/Documents/UniNotes/Y1S1/STAT1008/ST1008AS1/")
 aq = read.csv("airquality.csv")
-hist(aq$Temp)
+hist(aq$Temp, breaks=20)
 #Save currently visible plot
+boxplot(Ozone~Month,data=aq, main="Ozone per Month", ylab="Ozone Density", xlab="Month")
 dev.print(png, file="histtemp.png")
+table(aq$Month, aq$Ozone)
